f64d1d5d05a317645778a4a13a85d1540db2cea8
Andrey Vakhutinskiy <avakhutinskiy@gmail.com>
2020-08-14 22:54:11 +0000

    changed2.py was created

--- changed2.py
+++ changed2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os
+import subprocess
 import sys
 
 if len(sys.argv) == 1:
@@ -10,12 +10,15 @@ elif len(sys.argv) > 2:
     sys.exit(1)
 else:
     path = sys.argv[1]
-path = "cd " + path
-bash_command = [path, "git status"]
-result_os = os.popen(' && '.join(bash_command)).read()
+bash_command = ["cd " + path, "git status"]
+if subprocess.call(' && '.join(bash_command), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL):
+    print("Этот каталог не является каталогом git")
+    sys.exit()
+result_os = subprocess.run(' && '.join(bash_command), shell=True, stdout=subprocess.PIPE, text=True)
 is_change = False
-for result in result_os.split('\n'):
+for result in result_os.stdout.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print(prepare_result)
         break
+
