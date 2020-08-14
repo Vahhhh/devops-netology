60cf9bb7659c8965e2502409f6775dc79e620836
Andrey Vakhutinskiy <avakhutinskiy@gmail.com>
2020-08-14 20:28:25 +0000

    [04-script-02-py] file add

---
+++ changed2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+from sys import argv
+
+if len(sys.argv) == 1:
+	path = "~/devops-netology"
+elif len(sys.argv) > 2:
+	print("Ошибка. Слишком много параметров.")
+	sys.exit(1)
+else
+	path = sys.argv[1]
+
+bash_command = ["cd ~/devops-netology", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = False
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
+        break
+
