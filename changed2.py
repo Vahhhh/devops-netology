a8e92ca248178709bbb8d23fd48ce6ffa53d22c9
Andrey Vakhutinskiy <avakhutinskiy@gmail.com>
2020-08-27 11:41:19 +0000

    the best of changed2.py :)

--- changed2.py
+++ changed2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+#just comment to change file
 
 import subprocess
 import sys
+import os
 
 if len(sys.argv) == 1:
     path = "~/devops-netology"
@@ -11,14 +13,13 @@ elif len(sys.argv) > 2:
 else:
     path = sys.argv[1]
 bash_command = ["cd " + path, "git status"]
-if subprocess.call(' && '.join(bash_command), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL):
+result_os = subprocess.run(' && '.join(bash_command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+if result_os.returncode != 0:
     print("Этот каталог не является каталогом git")
     sys.exit()
-result_os = subprocess.run(' && '.join(bash_command), shell=True, stdout=subprocess.PIPE, text=True)
 is_change = False
 for result in result_os.stdout.split('\n'):
     if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', '')
+        prepare_result = os.getcwd() + '/' + result.replace('\tmodified:   ', '')
         print(prepare_result)
-        break
 
